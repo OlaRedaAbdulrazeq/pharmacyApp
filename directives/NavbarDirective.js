ab5d8bc65da7fac7010eb309b605cdc7e39f9d3f
OlaRedaAbdulrazeq <olaabdelrazeq@std.mans.edu.eg>
2026-03-08 05:53:20 +0200

    colored the care part in the navbar logo name to be black

--- directives/NavbarDirective.js
+++ directives/NavbarDirective.js
@@ -1,7 +1,7 @@
 adminApp.directive('customNavbar',function(){
     return{
         template: `<nav class="d-flex justify-content-between align-items-baseline px-md-5 py-2 position-sticky p-1" style='cursor:pointer'>
-                    <div><span class='text-blue'>PHARMACARE</span></div>
+                    <div><span class='text-blue'>PHARMA<span class='text-dark'>CARE</span> </span></div>
                     <div class='pb-4'>
                         <i class="bi bi-person fs-3" data-bs-toggle="dropdown"></i>
                         <span><b>Hello,Admin</b></span>
